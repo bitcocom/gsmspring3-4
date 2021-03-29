71f85f589b9d9618e63cce1b35ad57007b2c239d
GSM <GSM@DESKTOP-UUM6D3I>
2021-03-29 10:31:17 +0900

    gsm4

--- gsmspring3-4/src/kr/gsm/model/MemberDAO.java
+++ gsmspring3-4/src/kr/gsm/model/MemberDAO.java
@@ -7,9 +7,25 @@ public class MemberDAO {
    private ResultSet rs; //  select SQL->결과를 저장하는객체
    // 연결객체를 생성하는 동작
    public void getConnect() {
-	   String url="jdbc:mysql://localhost:3306/gsm4"; // DB접속URL
+	   String url="jdbc:mysql://localhost:3306/gsm4"; // DB접속URL(프로토콜)
 	   String user="root";
 	   String password="12345";
 	   //---------------------------------------------
-   }   
+	   //=new com.mysql.jdbc.Driver();	      
+	    try {
+		// 동적로딩(실행시점에서 객체를 찾고->생성)   
+		Class.forName("com.mysql.jdbc.Driver");
+		conn=DriverManager.getConnection(url, user, password);
+	    } catch (Exception e) {
+		  e.printStackTrace();
+	    } 
+   }
+   // 회원전체리스트(List<MemberVO>) 가져오기
+   public void memberList() {
+	   String SQL="select * from member";
+	   getConnect(); // DB연결
+	   
+	   
+   }
+   
 }
