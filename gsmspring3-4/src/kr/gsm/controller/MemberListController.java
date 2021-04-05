cd3812d7d0ce76cded0508adbed39415813ca52e
GSM <GSM@DESKTOP-UUM6D3I>
2021-04-05 12:04:56 +0900

    gsm4

--- gsmspring3-4/src/kr/gsm/controller/MemberListController.java
+++ gsmspring3-4/src/kr/gsm/controller/MemberListController.java
@@ -46,9 +46,13 @@ public class MemberListController extends HttpServlet {
 		    out.println("<td>"+vo.getEmail()+"</td>");
 		    out.println("</tr>");
 	    }
+	    out.println("<tr>");
+	    out.println("<td colspan='7'>");
+	    out.println("<a href='member/memberRegister.html'>회원가입</a>");
+	    out.println("</td>");
+	    out.println("</tr>");
 	    out.println("</table>");
 	    out.println("</body>");
-	    out.println("</html>");
-		
+	    out.println("</html>");		
 	}
 }
